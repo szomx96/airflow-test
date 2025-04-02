8827537312f8a432a9feadfb376cc5f96ef8062a
Dominika <jurczyk.dom@gmail.com>
2025-04-02 17:40:20 +0200

    Update dag.py

--- dags/dag.py
+++ dags/dag.py
@@ -3,7 +3,7 @@ import datetime
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 
- with DAG(
+with DAG(
     dag_id="my_dag_name",
     start_date=datetime.datetime(2021, 1, 1),
     schedule="@daily",
